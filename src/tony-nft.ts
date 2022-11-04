3998f94a6d0b0abb61b68e4e5cd30582a47f919c
Simon Emanuel Schmid <simon@schmid.io>
2022-11-04 14:40:55 -0700

    Hello world

--- src/tony-nft.ts
+++ src/tony-nft.ts
@@ -4,35 +4,35 @@ import {
   OwnershipTransferred as OwnershipTransferredEvent,
   Paused as PausedEvent,
   Transfer as TransferEvent,
-  Unpaused as UnpausedEvent
-} from "../generated/TonyNFT/TonyNFT"
+  Unpaused as UnpausedEvent,
+} from "../generated/TonyNFT/TonyNFT";
 import {
   Approval,
   ApprovalForAll,
   OwnershipTransferred,
   Paused,
   Transfer,
-  Unpaused
-} from "../generated/schema"
+  Unpaused,
+} from "../generated/schema";
 
 export function handleApproval(event: ApprovalEvent): void {
   let entity = new Approval(
     event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.owner = event.params.owner
-  entity.approved = event.params.approved
-  entity.tokenId = event.params.tokenId
-  entity.save()
+  );
+  entity.owner = event.params.owner;
+  entity.approved = event.params.approved;
+  entity.tokenId = event.params.tokenId;
+  entity.save();
 }
 
 export function handleApprovalForAll(event: ApprovalForAllEvent): void {
   let entity = new ApprovalForAll(
     event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.owner = event.params.owner
-  entity.operator = event.params.operator
-  entity.approved = event.params.approved
-  entity.save()
+  );
+  entity.owner = event.params.owner;
+  entity.operator = event.params.operator;
+  entity.approved = event.params.approved;
+  entity.save();
 }
 
 export function handleOwnershipTransferred(
@@ -40,34 +40,35 @@ export function handleOwnershipTransferred(
 ): void {
   let entity = new OwnershipTransferred(
     event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
-  entity.save()
+  );
+  entity.previousOwner = event.params.previousOwner;
+  entity.newOwner = event.params.newOwner;
+  entity.save();
 }
 
 export function handlePaused(event: PausedEvent): void {
   let entity = new Paused(
     event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.account = event.params.account
-  entity.save()
+  );
+  entity.account = event.params.account;
+  entity.save();
 }
 
 export function handleTransfer(event: TransferEvent): void {
   let entity = new Transfer(
     event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.tokenId = event.params.tokenId
-  entity.save()
+  );
+  entity.from = event.params.from;
+  entity.to = event.params.to;
+  entity.tokenId = event.params.tokenId;
+  entity.blockNumber = event.block.number;
+  entity.save();
 }
 
 export function handleUnpaused(event: UnpausedEvent): void {
   let entity = new Unpaused(
     event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.account = event.params.account
-  entity.save()
+  );
+  entity.account = event.params.account;
+  entity.save();
 }
